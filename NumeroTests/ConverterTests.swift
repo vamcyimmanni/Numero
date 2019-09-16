8a6de1eeae892d56d3822d13b82c16fe6d837633
Immani, Vamsi (ES) <Vamsi.Immani@ADP.com>
2019-09-16 15:37:49 +0530

    fastlane travis yml

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -24,7 +24,7 @@ class ConverterTests: XCTestCase {
     }
     
     func testConversionForOne() {
-        let result = converter.convert(1)
+        let result = converter.convert(I)
         XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
     }
 }
